58e52081d8c19ab4a4f42d475793d9eb16e8cf55
Noppol Pilukruangdet <knookcs08@gmail.com>
2015-05-15 12:46:16 +0700

    first commit

---
+++ GuestBook_Data/GuestBookEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBook_Data
+{
+    public class GuestBookEntry
+        : Microsoft.WindowsAzure.Storage.Table.TableEntity
+    {
+        public GuestBookEntry()
+        {
+            PartitionKey = DateTime.UtcNow.ToString("MMddyyyy");
+            RowKey = string.Format("{0:10}_{1}",
+                DateTime.MaxValue.Ticks - DateTime.UtcNow.Ticks,
+                Guid.NewGuid());
+        }
+
+        public string Message { get; set; }
+        public string GuestName { get; set; }
+        public string PhotoUrl { get; set; }
+        public string ThumbnailUrl { get; set; }
+    }
+}
---
+++ GuestBook_WebRole/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace GuestBook_WebRole.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
